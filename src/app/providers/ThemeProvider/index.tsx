327cc763e4622c3fc6a99d6d2154b703e0e6cd5a
Sergey Kachaliuk <sergeykachaliukdev@gmail.com>
2025-02-14 17:34:19 +0300

    fix: add window?. prefix for localStorage

--- src/app/providers/ThemeProvider/index.tsx
+++ src/app/providers/ThemeProvider/index.tsx
@@ -5,23 +5,22 @@ import { THEME_NAMES } from "@/shared/constants";
 import { FC, PropsWithChildren, useEffect, useState } from "react";
 
 const ThemeProvider: FC<PropsWithChildren> = ({ children }) => {
-  // const [themeName, setThemeName] = useState(() => {
-  //   const item = localStorage.getItem("theme");
-  //   return item ? JSON.parse(item) : THEME_NAMES.LIGHT;
-  // });
-  const [themeName, setThemeName] = useState(THEME_NAMES.LIGHT);
+  const [themeName, setThemeName] = useState(() => {
+    const item = window?.localStorage.getItem("theme");
+    return item ? JSON.parse(item) : THEME_NAMES.LIGHT;
+  });
 
   const toggleThemeHandler = () => {
     const newTheme =
       themeName === THEME_NAMES.LIGHT ? THEME_NAMES.DARK : THEME_NAMES.LIGHT;
 
     setThemeName(newTheme);
-    // localStorage.setItem("theme", JSON.stringify(newTheme));
+    window?.localStorage.setItem("theme", JSON.stringify(newTheme));
   };
 
   useEffect(() => {
     document.documentElement.setAttribute("data-theme", themeName);
-    // localStorage.setItem("theme", JSON.stringify(themeName));
+    window?.localStorage.setItem("theme", JSON.stringify(themeName));
   }, [themeName]);
 
   return (
