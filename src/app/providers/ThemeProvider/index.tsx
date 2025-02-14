3caa5d40438b074f8cd27edc6d0402fee6821ee3
Sergey Kachaliuk <sergeykachaliukdev@gmail.com>
2025-02-14 10:12:21 +0300

    fix: remove local storage functions

--- src/app/providers/ThemeProvider/index.tsx
+++ src/app/providers/ThemeProvider/index.tsx
@@ -4,22 +4,10 @@ import { ThemeContext } from "@/app/providers/ThemeProvider/context";
 import { THEME_NAMES } from "@/shared/constants";
 import { FC, PropsWithChildren, useEffect, useState } from "react";
 
-const getItemFromLocalStorage = (key: string) => {
-  const item = localStorage.getItem(key);
-  return item ? JSON.parse(item) : null;
-};
-
-const setItemToLocalStorage = (key: string, value: any) => {
-  localStorage.setItem(key, JSON.stringify(value));
-};
-
-const removeItemFromLocalStorage = (key: string) => {
-  localStorage.removeItem(key);
-};
-
 const ThemeProvider: FC<PropsWithChildren> = ({ children }) => {
   const [themeName, setThemeName] = useState(() => {
-    return getItemFromLocalStorage("theme") || THEME_NAMES.LIGHT;
+    const item = localStorage.getItem("theme");
+    return item ? JSON.parse(item) : THEME_NAMES.LIGHT;
   });
 
   const toggleThemeHandler = () => {
@@ -27,12 +15,12 @@ const ThemeProvider: FC<PropsWithChildren> = ({ children }) => {
       themeName === THEME_NAMES.LIGHT ? THEME_NAMES.DARK : THEME_NAMES.LIGHT;
 
     setThemeName(newTheme);
-    setItemToLocalStorage("theme", newTheme);
+    localStorage.setItem("theme", JSON.stringify(newTheme));
   };
 
   useEffect(() => {
     document.documentElement.setAttribute("data-theme", themeName);
-    setItemToLocalStorage("theme", themeName);
+    localStorage.setItem("theme", JSON.stringify(themeName));
   }, [themeName]);
 
   return (
